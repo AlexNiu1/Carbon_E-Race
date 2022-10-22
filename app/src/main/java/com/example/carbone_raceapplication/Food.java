78f3e4eebbb2d9e3e0f0a26d29d07923a036b56d
AlexNiu <alexniupdx@gmail.com>
2022-10-22 12:33:26 -0700

    connections

--- app/src/main/java/com/example/carbone_raceapplication/Food.java
+++ app/src/main/java/com/example/carbone_raceapplication/Food.java
@@ -1,18 +1,31 @@
 package com.example.carbone_raceapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.carbone_raceapplication.databinding.ActivityFoodBinding;
+
+public class Food extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityFoodBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_food);
         ImageView homebutton = findViewById(R.id.home);
         homebutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
