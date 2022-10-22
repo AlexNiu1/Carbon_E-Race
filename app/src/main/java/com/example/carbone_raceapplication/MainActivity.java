0d0c2ab5bd3e069786324fef95c877d553aac40f
AlexNiu <alexniupdx@gmail.com>
2022-10-22 12:34:27 -0700

    Revert "connections"
    This reverts commit 78f3e4eebbb2d9e3e0f0a26d29d07923a036b56d.

--- app/src/main/java/com/example/carbone_raceapplication/MainActivity.java
+++ app/src/main/java/com/example/carbone_raceapplication/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.carbone_raceapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,40 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView homebutton = findViewById(R.id.home);
-        homebutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                // start the game
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-            }
-        });
-        ImageView foodbutton = findViewById(R.id.food);
-        foodbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                // start the game
-                startActivity(new Intent(MainActivity.this, Food.class));
-            }
-        });
-        ImageView transportationbutton = findViewById(R.id.transportation);
-        transportationbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                // start the game
-                startActivity(new Intent(MainActivity.this, Transportation.class));
-            }
-        });
-        ImageView boardbutton = findViewById(R.id.leaderboard);
-        boardbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                // start the game
-                startActivity(new Intent(MainActivity.this, Leaderboard.class));
-            }
-        });
-        ImageView settingsbutton = findViewById(R.id.settings);
-        settingsbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                // start the game
-                startActivity(new Intent(MainActivity.this, Settings.class));
-            }
-        });
     }
 }
