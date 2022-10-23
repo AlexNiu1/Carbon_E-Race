0040f434e7f66d6d355535d9b1bcea068f8938b8
AlexNiu <alexniupdx@gmail.com>
2022-10-22 21:30:02 -0700

    leaderboard

--- app/src/main/java/com/example/carbone_raceapplication/Leaderboard.java
+++ app/src/main/java/com/example/carbone_raceapplication/Leaderboard.java
@@ -22,6 +22,7 @@ public class Leaderboard extends AppCompatActivity {
         if (league == null){
             findViewById(R.id.leaguesize).setEnabled(false);
             findViewById(R.id.leaveleague).setEnabled(false);
+            findViewById(R.id.leaveleague).setVisibility(View.INVISIBLE);
         }
         else{
             EditText room = findViewById(R.id.room);
@@ -37,7 +38,7 @@ public class Leaderboard extends AppCompatActivity {
         Button createleague = findViewById(R.id.createleague);
         createleague.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
-                EditText name = findViewById(R.id.leaguename)
+                EditText name = findViewById(R.id.leaguename);
                 String newleague = name.getText().toString();
                 if (!newleague.matches("[a-zA-Z0-9]*") || prefs.getString("leagues", "").contains(newleague)){
                     name.setText("Error: League name is either not valid or already in use");
@@ -52,9 +53,12 @@ public class Leaderboard extends AppCompatActivity {
                     EditText size = findViewById(R.id.leaguesize);
                     size.setText("League size: 5");
                     findViewById(R.id.leaveleague).setEnabled(true);
+                    findViewById(R.id.leaveleague).setVisibility(View.VISIBLE);
                     findViewById(R.id.leaguename).setEnabled(false);
                     findViewById(R.id.createleague).setEnabled(false);
+                    findViewById(R.id.createleague).setVisibility(View.INVISIBLE);
                     findViewById(R.id.joinleague).setEnabled(false);
+                    findViewById(R.id.joinleague).setVisibility(View.INVISIBLE);
                     findViewById(R.id.notinleague).setEnabled(false);
                 }
             }
@@ -77,9 +81,12 @@ public class Leaderboard extends AppCompatActivity {
                     EditText size = findViewById(R.id.leaguesize);
                     size.setText("League size: 5");
                     findViewById(R.id.leaveleague).setEnabled(true);
+                    findViewById(R.id.leaveleague).setVisibility(View.VISIBLE);
                     findViewById(R.id.leaguename).setEnabled(false);
                     findViewById(R.id.createleague).setEnabled(false);
+                    findViewById(R.id.createleague).setVisibility(View.INVISIBLE);
                     findViewById(R.id.joinleague).setEnabled(false);
+                    findViewById(R.id.joinleague).setVisibility(View.INVISIBLE);
                     findViewById(R.id.notinleague).setEnabled(false);
                 }
             }
@@ -94,43 +101,42 @@ public class Leaderboard extends AppCompatActivity {
                 room.setText("Your current league: none");
                 findViewById(R.id.leaguesize).setEnabled(false);
                 findViewById(R.id.leaveleague).setEnabled(false);
+                findViewById(R.id.leaveleague).setVisibility(View.INVISIBLE);
                 findViewById(R.id.leaguename).setEnabled(true);
                 findViewById(R.id.createleague).setEnabled(true);
+                findViewById(R.id.createleague).setVisibility(View.VISIBLE);
                 findViewById(R.id.joinleague).setEnabled(true);
+                findViewById(R.id.joinleague).setVisibility(View.VISIBLE);
                 findViewById(R.id.notinleague).setEnabled(true);
             }
         });
-        ImageView homebutton = findViewById(R.id.home);
+        Button homebutton = findViewById(R.id.homeButton);
         homebutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(Leaderboard.this, MainActivity.class));
             }
         });
-        ImageView homebutton = findViewById(R.id.home);
-        homebutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                startActivity(new Intent(Leaderboard.this, MainActivity.class));
-            }
-        });
-        ImageView foodbutton = findViewById(R.id.food);
+        Button foodbutton = findViewById(R.id.foodButton);
         foodbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(Leaderboard.this, Food.class));
             }
         });
-        ImageView transportationbutton = findViewById(R.id.transportation);
+        Button transportationbutton = findViewById(R.id.transportationButton);
         transportationbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(Leaderboard.this, Transportation.class));
             }
         });
-        ImageView boardbutton = findViewById(R.id.leaderboard);
+        Button boardbutton = findViewById(R.id.leaderboardButton);
         boardbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(Leaderboard.this, Leaderboard.class));
             }
         });
-        ImageView settingsbutton = findViewById(R.id.settings);
+
+
+        Button settingsbutton = findViewById(R.id.settingsButton);
         settingsbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(Leaderboard.this, Settings.class));
